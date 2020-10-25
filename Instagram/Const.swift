abe45cc130e5293026d20f925d2e6d5a59d14862
03pink12 <nyankfood@gmail.com>
2020-10-25 22:11:52 +0900

    「9. タイムライン画面」まで実装

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by mac on 2020/10/25.
+//  Copyright © 2020 03pink12. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
